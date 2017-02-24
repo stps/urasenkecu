036b3f2e09daeccde9d0a4693d2ce02e513571e5
Felix <felixwang01@gmail.com>
2017-02-24 02:57:27 -0600

    gallery page

--- gallery.php
+++ gallery.php
@@ -17,6 +17,24 @@
     <link href="css/members.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Alegreya' rel='stylesheet' type='text/css'>
 
+    <style type="text/css">
+    .container ul {
+      text-align: center;
+    }
+    .container ul li {
+      list-style-type: none;
+      margin-right: 10px;
+      margin-bottom: 10px;
+display: inline-block;
+    }
+    .thumb-cropped {
+      width: 200px;
+      height: 200px;
+              background-position: center center;
+              background-repeat: no-repeat;
+    }
+    </style>
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -54,7 +72,7 @@
     </header>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="images/gallery.jpg" data-width: style="height: 400px; margin-top: 40px;">
+    <div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="images/open_house_fa2016.jpg" data-width: style="height: 400px; margin-top: 40px;">
       <div class="jumbotron">
       <h2>Gallery</h2>
     </div>
@@ -67,16 +85,28 @@
 
       <!--<h3>Members</h3>-->
       <hr class="half-rule">
-      <p>
-      Gallery content
-      </p>
+      <div align="center">
+      <ul>
+      <?php
+        $dirname = "gallery/";
+        $thumbname = "thumb/";
+        $images = scandir($dirname);
+        $ignore = Array(".", "..");
+        foreach ($images as $currimg) {
+          if (!in_array($currimg, $ignore)) {
+            echo "<li><a href='".$dirname.$currimg."'><div class=\"thumb-cropped\" style=\"background-image: url('".$thumbname.substr_replace($currimg,"s.jpg",-5)."');\"></div></a></li>";
+          }
+        }
+      ?>
+      </ul>
+      </div>
       <hr class="half-rule">
     </div>
     <!-- </div> /container -->
 
     <footer class="footer">
       <div class="container-f">
-        <p class="text-muted">© 2015 Chado Urasenke Tankokai Urbana-Champaign Association</p>
+        <p class="text-muted">© 2017 Chado Urasenke Tankokai Urbana-Champaign Association</p>
       </div>
     </footer>
 
